e6690087b7f4898c1633e85c5c694a77b7e2bbb6
T-Cuong <trongcuong17pfiev@gmail.com>
2023-02-14 19:30:49 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <PageTitle>Todo list</PageTitle>
+      <div className={styles.app__wrapper}>
+        <AppHeader />
+        <AppContent />
+      </div>
+    </div>
+    <Toaster
+    position='bottom-right'
+    toastOptions={{
+      style: {
+        fontSize: '1.4rem',
+      }
+    }}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/GlobalStyles.css';
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store = {store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
